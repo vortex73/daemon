2998b2d8bc0ee7fb13455e5d4b69e415ffd81cb3
vortex73 <nsreekumar6@gmail.com>
2023-04-02 12:45:08 +0530

    fork

--- main.c
+++ main.c
@@ -7,5 +7,11 @@ int main (int argc, char *argv[])
 {
   int id = fork();
   printf("Hello World from id : %d\n",id);
+  if (id == 0) {
+    printf("Hello from the child process");
+  
+  } else {
+  printf("Hello From the main process\n");
+  }
   return 0;
 }
