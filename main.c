938eb545113517683f5d4da437c9bb6c2754ce62
vortex73 <nsreekumar6@gmail.com>
2023-04-04 22:54:44 +0530

    umtp file handling

--- main.c
+++ main.c
@@ -5,22 +5,12 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main (int argc, char *argv[])
+int main()
 {
   int id = fork();
-  int n ;
-  if (id == 0)
-  {
-    n = 1;
-  }else{
-    n=6;
+  if (id==0) {
+    sleep(1);
+  
   }
-  if (id != 0)
-  {
-    wait();
-  }
-  for (int i=n;i<n+5;i++)
-    {
-      printf("%d",i);
-    }
+  printf("current id: %d parent id: %d\n",getpid(),getppid());
 }
