33fb3a650256a40bafd2c9054a0d03ac2e1e7f11
vortex73 <nsreekumar6@gmail.com>
2023-04-08 03:06:17 +0530

    Latest login obtained

--- new.c
+++ new.c
@@ -7,11 +7,16 @@ int main() {
     struct utmp currentRecord;
 
     file = fopen("/var/log/wtmp", "rb");
+    fseek(file,0,SEEK_END);
+    long int pos = ftell(file);
+    pos -= sizeof(currentRecord);
+    fseek(file,pos,SEEK_SET);
     if (file) {
         while (fread(&currentRecord, sizeof(currentRecord), 1, file) == 1) {   // fread function returns the number of elements that were successfully read .
                                                                                 // Since we requested only 1 to be read, this condition must hold true 
             if (currentRecord.ut_type == USER_PROCESS) {
                 time_t loginTime = currentRecord.ut_tv.tv_sec;
+                
                 printf("Login time: %s", ctime(&loginTime));
             }
         }
