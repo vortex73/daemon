bb0dad3c09f40667ee6c771e92fde62e4a688d86
vortex73 <nsreekumar6@gmail.com>
2023-04-08 02:33:50 +0530

    gitignore added

--- new.c
+++ new.c
@@ -8,7 +8,8 @@ int main() {
 
     file = fopen("/var/log/wtmp", "rb");
     if (file) {
-        while (fread(&currentRecord, sizeof(currentRecord), 1, file) == 1) {
+        while (fread(&currentRecord, sizeof(currentRecord), 1, file) == 1) {   // fread function returns the number of elements that were successfully read .
+                                                                                // Since we requested only 1 to be read, this condition must hold true 
             if (currentRecord.ut_type == USER_PROCESS) {
                 time_t loginTime = currentRecord.ut_tv.tv_sec;
                 printf("Login time: %s", ctime(&loginTime));
