82d3db04c370d0e5f7231091d13715678f5be1d4
vortex73 <nsreekumar6@gmail.com>
2023-05-01 01:24:40 +0530

    deepdive on daemon

---
+++ testing.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+int main (int argc, char *argv[])
+{
+  FILE * fp = NULL;
+  pid_t PId = 0;
+  pid_t sid = 0;
+
+  return 0;
+}
